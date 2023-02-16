de2145216ead7b431ef84663692ea3b87fd8f948
Jared Mejia <jaredmejia4440@gmail.com>
2023-02-15 20:07:15 -0600

    uninstall cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(cookieParser());
 
   app.useGlobalPipes(
     new ValidationPipe({
